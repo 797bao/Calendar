66710f79a46b4b8448148c77f2ae4e5e6b1ebd9c
Bao <797nguyenbao@gmail.com>
2022-09-05 11:33:52 -0700

    Header change with swipe context

--- components/headerStyles.js
+++ components/headerStyles.js
@@ -1,57 +1,59 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    height: 56,
-    justifyContent: 'center',
+    height: Dimensions.get("screen").height * 0.05,
+    justifyContent: "center",
     padding: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     elevation: 3,
     zIndex: 999,
   },
   text_row: {
-    flexDirection: 'row',
-    alignItems:'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   center: {
     paddingLeft: 10,
     flexGrow: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   circle: {
     width: 30,
-    height: 30,
-    borderRadius: 30/2,
-    alignItems:'center',
-    justifyContent: 'center',
+    height: 22,
+    borderRadius: 30 / 2,
+    alignItems: "center",
+    justifyContent: "center",
   },
   day_box: {
-    flexDirection: 'column',
-    alignItems:'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
     margin: 0,
     padding: 0,
   },
   day: {
     margin: 0,
     padding: 0,
-    fontWeight: 'bold',
-    fontSize: 15,
+    fontWeight: "bold",
+    fontSize: 18,
   },
   dotw: {
     margin: 0,
     padding: 0,
-    textAlign: 'center',
-    fontSize: 10,
+    textAlign: "center",
+    fontSize: 13,
     paddingBottom: 1,
   },
   month: {
-    fontSize: 20,
+    paddingTop: 10,
+    fontSize: 23,
     paddingLeft: 10,
-    fontWeight: '300'
+    fontWeight: "300",
   },
   img: {
     paddingRight: 20,
@@ -60,12 +62,15 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   arrow_down: {
+    marginTop: 10,
+    paddingTop: 10,
     paddingLeft: 15,
   },
   arrow_up: {
+    marginTop: 10,
     paddingLeft: 15,
-    transform:[{rotate: '180 deg'}]
-  }
+    transform: [{ rotate: "180 deg" }],
+  },
 });
 
-export default styles
+export default styles;
