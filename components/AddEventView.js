93bc6b3099b4c1adf8b910f40c5e8ae4e60196f9
Bao <797nguyenbao@gmail.com>
2022-10-05 13:40:15 -0700

    quick test push

--- components/AddEventView.js
+++ components/AddEventView.js
@@ -10,10 +10,13 @@ import {
 //import NativeColorPicker from 'native-color-picker';
 import ColorPalette from "react-native-color-palette";
 import { Button } from "react-native-paper";
+import procData from "../services/procData";
 
 const colors = ["#C0392B", "#E74C3C", "#9B59B6", "#8E44AD", "#2980B9"];
+import { Dimensions } from "react-native";
+let hourSize = Dimensions.get("window").height / 13.34;
 
-const AddEventView = () => {
+const AddEventView = (props) => {
   const [chosenStartDate, setChosenStartDate] = useState(new Date());
   const [chosenEndDate, setChosenEndDate] = useState(new Date());
   const [title, onChangeTitle] = React.useState("Event");
@@ -27,6 +30,36 @@ const AddEventView = () => {
     console.log("Color: " + color);
     console.log("Start Date: " + chosenStartDate);
     console.log("End Date: " + chosenEndDate);
+    console.log("PROPS ");
+    console.log(props);
+
+    let newData = {
+      title: title,
+      subtitle: subtitle,
+      start: new Date(2022, 8, 28, 1, 21),
+      end: new Date(2022, 8, 28, 7, 20),
+      color: color,
+    };
+
+    if (
+      //this map has no entry for this day
+      props.route.params.loggedItems.get(chosenStartDate.toString()) == null
+    ) {
+      props.route.params.loggedItems.set(
+        //add new entry
+        chosenStartDate.toString(),
+        procData([newData], hourSize)
+      );
+    } else {
+      //push new entry to existing array
+      props.route.params.loggedItems
+        .get(chosenStartDate.toString())
+        .push(newData);
+    }
+
+    console.log("PROPS 2 " + newData);
+    console.log("after " + props.route.params.loggedItems);
+    console.log(props);
   };
 
   return (
