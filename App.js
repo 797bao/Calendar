90f2500767df1c2bf6901a23c3f366bb1d82f1f2
Bao <797nguyenbao@gmail.com>
2022-11-20 16:27:07 -0800

    Merge Journal & Calendar, dark to white theme

--- App.js
+++ App.js
@@ -1,45 +1,159 @@
-import * as eva from "@eva-design/eva"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { NavigationContainer } from "@react-navigation/native"
-import { ApplicationProvider, BottomNavigation, BottomNavigationTab } from "@ui-kitten/components"
-import React from "react"
-import { StyleSheet } from "react-native"
-import AllJournals from "./components/AllJournals"
-import CreateNote from "./components/CreateNote"
-import Note from "./components/Note"
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const { Navigator, Screen } = createBottomTabNavigator()
+import MonthlyView from "./components/MonthlyView";
+import DailyView from "./components/DailyView";
+import AddEventView from "./components/AddEventView";
+import procData from "./services/procData";
+import { Dimensions } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  BottomNavigation,
+  BottomNavigationTab,
+} from "@ui-kitten/components";
+import AllJournals from "./components/AllJournals";
+import CreateNote from "./components/CreateNote";
+import Note from "./components/Note";
+
+let hourSize = Dimensions.get("window").height / 13.34;
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const loggedItems = new Map();
+loggedItems.set(
+  new Date(2022, 8, 25).toString(),
+  procData(
+    [
+      {
+        title: "TEST",
+        subtitle: "",
+        start: new Date(2022, 8, 25, 2, 21),
+        end: new Date(2022, 8, 25, 4, 0),
+        color: "#aa0000",
+      },
+    ],
+    hourSize
+  )
+);
+
+loggedItems.set(
+  new Date(2022, 8, 26).toString(),
+  procData(
+    [
+      {
+        title: "Lunch Appointment",
+        subtitle: "With John",
+        start: new Date(2022, 8, 26, 1, 21),
+        end: new Date(2022, 8, 26, 7, 20),
+        color: "#390099",
+      },
+      {
+        title: "Lunch Appointment",
+        subtitle: "With Bao",
+        start: new Date(2022, 8, 26, 4, 20),
+        end: new Date(2022, 8, 26, 5, 20),
+        color: "#ffff00",
+      },
+    ],
+    hourSize
+  )
+);
+
+loggedItems.set(
+  new Date(2022, 8, 28).toString(),
+  procData(
+    [
+      {
+        title: "Lunch Appointment",
+        subtitle: "With John",
+        start: new Date(2022, 8, 28, 1, 21),
+        end: new Date(2022, 8, 28, 6, 0),
+        color: "#ff0000",
+      },
+    ],
+    hourSize
+  )
+);
 
 const BottomTabBar = ({ navigation, state }) => (
-    <BottomNavigation selectedIndex={state.index} onSelect={(index) => navigation.navigate(state.routeNames[index])}>
-        <BottomNavigationTab title="Create" />
-        <BottomNavigationTab title="All Notes" />
-    </BottomNavigation>
-)
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}
+    tabBarVisible={false}
+    headerShown={false}
+  >
+    <BottomNavigationTab title="Create" />
+    <BottomNavigationTab title="All Notes" />
+  </BottomNavigation>
+);
 
 const TabNavigator = () => (
-    <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
-        <Screen name="Create" component={CreateNote} />
-        <Screen name="AllJournals" component={AllJournals} />
-        <Screen name="Note" component={Note} />
-    </Navigator>
-)
+  <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
+    <Screen name="Create" component={CreateNote} />
+    <Screen name="AllJournals" component={AllJournals} headerShown={false} />
+    <Screen name="Note" component={Note} />
+  </Navigator>
+);
 
-export default function App() {
-    return (
-        <ApplicationProvider {...eva} theme={eva.dark}>
-            <NavigationContainer>
-                <TabNavigator />
-            </NavigationContainer>
-        </ApplicationProvider>
-    )
+function JournalScreen({ navigation }) {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <TabNavigator />
+    </ApplicationProvider>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-})
+function CalendarScreen() {
+  return (
+    <Stack.Navigator initialRouteName="DailyView">
+      <Stack.Screen name="MonthlyView" component={MonthlyView} />
+      <Stack.Screen
+        name="DailyView"
+        props={loggedItems}
+        component={DailyView}
+        options={{ headerShown: false }}
+        initialParams={{ loggedItems: loggedItems }}
+      />
+      {(props) => <DailyView {...props} loggedItems={loggedItems} />}
+      <Stack.Screen
+        name="AddEventView"
+        component={AddEventView}
+        initialParams={{ loggedItems: loggedItems }}
+      />
+      {(props) => <DailyView {...props} loggedItems={loggedItems} />}
+    </Stack.Navigator>
+  );
+}
+
+function MetricsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button
+        title="Metrics Screen Work In Progress"
+        onPress={() => console.log("placeholder")}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Calendar"
+        screenOptions={{ headerShown: false }}
+      >
+        <Tab.Screen name="Journal" component={JournalScreen} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="Metrics" component={MetricsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- components/AllJournals.js
+++ components/AllJournals.js
@@ -1,66 +1,63 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useFocusEffect, useNavigation } from "@react-navigation/native"
-import { Divider, List, ListItem, Text } from "@ui-kitten/components"
-import React, { useState } from "react"
-import { StyleSheet, View } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { Divider, List, ListItem, Text } from "@ui-kitten/components";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function AllJournals() {
-	const [ notes, setNotes ] = useState([])
-	const navigation = useNavigation()
+  const [notes, setNotes] = useState([]);
+  const navigation = useNavigation();
 
-	useFocusEffect(
-		React.useCallback(() => {
-			getNotes()
-		}, [])
-	)
+  useFocusEffect(
+    React.useCallback(() => {
+      getNotes();
+    }, [])
+  );
 
-	const getNotes = () => {
-		AsyncStorage.getItem("NOTES").then((notes) => {
-			setNotes(JSON.parse(notes))
-		})
-	}
+  const getNotes = () => {
+    AsyncStorage.getItem("NOTES").then((notes) => {
+      setNotes(JSON.parse(notes));
+    });
+  };
 
-	const renderItem = ({ item, index }) => (
-		<ListItem 
-			title={<Text category="h5">{item}</Text>}
-			onPress={() =>
-				navigation.navigate("Note", {
-					singleNote: item
-				})}
-		/>
-	)
+  const renderItem = ({ item, index }) => (
+    <ListItem
+      title={<Text category="h5">{item}</Text>}
+      onPress={() =>
+        navigation.navigate("Note", {
+          singleNote: item,
+        })
+      }
+    />
+  );
 
-	return (
-		<View style={{ backgroundColor: "#222B45", flex: 1 }}>
-			<Text style={styles.title} category="h5">
-				Journal
-			</Text>
-			<List
-				style={styles.container}
-				data={notes.reverse()}
-				ItemSeparatorComponent={Divider}
-				renderItem={renderItem}
-			/>
-		</View>
-	
-	)
+  return (
+    <View style={{ backgroundColor: "#FFF", flex: 1 }}>
+      <List
+        style={styles.container}
+        data={notes.reverse()}
+        ItemSeparatorComponent={Divider}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		fontSize: 20,
-	},
+  container: {
+    fontSize: 20,
+  },
 
-	item: {
-		marginVertical: 4
-	},
-	title: {
-		padding: 2,
-		textAlign: "center",
-		marginTop: 40,
-		marginBottom: 10
-	},
-	notes: {
-		fontSize: 24
-	}
-})
+  item: {
+    marginVertical: 4,
+  },
+  title: {
+    padding: 2,
+    textAlign: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  notes: {
+    fontSize: 24,
+  },
+});
