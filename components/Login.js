e8fa757de5aa4a323988a162b57230d3def74073
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2022-12-01 23:23:31 -0800

    login screen

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
 import LoginScreen from "react-native-login-screen";
 
-<LoginScreen
-  logoImageSource={require("../assets/icon.png")}
-  onLoginPress={() => {}}
-  onSignupPress={() => {}}
-  onEmailChange={() => {}}
-  onPasswordChange={() => {}}
-/>
+const Login = (props) => {
+    return (
+    <SafeAreaView> 
+        <LoginScreen
+            logoImageSource={require("../assets/adaptive-icon.png")}
+            onLoginPress={() => {}}
+            onSignupPress={() => {}}
+            onEmailChange={() => {}}
+            onPasswordChange={() => {}}
+        />
+    </SafeAreaView>
+    )
+}
+
+export default Login;
+
